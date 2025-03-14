54bf8d21e7bf7f883119978b7a1ee008db5de680
Luiz Santos <lzg.lisboasantos5569@gmail.com>
2025-03-14 16:59:54 -0300

    Limpando dashboard

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -92,15 +92,6 @@ const AppHeader = () => {
               >
                 <CIcon className="me-2" icon={cilMoon} size="lg" /> Dark
               </CDropdownItem>
-              <CDropdownItem
-                active={colorMode === 'auto'}
-                className="d-flex align-items-center"
-                as="button"
-                type="button"
-                onClick={() => setColorMode('auto')}
-              >
-                <CIcon className="me-2" icon={cilContrast} size="lg" /> Auto
-              </CDropdownItem>
             </CDropdownMenu>
           </CDropdown>
           <li className="nav-item py-1">
--- src/components/DocsComponents.js
+++ src/components/DocsComponents.js
@@ -1,19 +1,11 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 
-import ComponentsImg from 'src/assets/images/components.webp'
-
 const DocsComponents = (props) => (
   <div className="bg-primary bg-opacity-10 border border-2 border-primary rounded mb-4">
     <div className="row d-flex align-items-center p-3 px-xl-4 flex-xl-nowrap">
       <div className="col-xl-auto col-12 d-none d-xl-block p-0">
-        <img
-          className="img-fluid"
-          src={ComponentsImg}
-          width="160px"
-          height="160px"
-          alt="CoreUI PRO hexagon"
-        />
+        <img className="img-fluid" src="" width="160px" height="160px" alt="CoreUI PRO hexagon" />
       </div>
       <div className="col-md col-12 px-lg-4">
         Our Admin Panel isn’t just a mix of third-party components. It’s{' '}
