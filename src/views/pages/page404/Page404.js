be53b1f08b9227e983758f899b2b2593d9a144e3
Luiz Santos <lzg.lisboasantos5569@gmail.com>
2025-03-19 14:14:50 -0300

    Criando Profile e traduzindo página 404

--- src/views/pages/page404/Page404.js
+++ src/views/pages/page404/Page404.js
@@ -1,15 +1,5 @@
+import { CCol, CContainer, CRow } from '@coreui/react'
 import React from 'react'
-import {
-  CButton,
-  CCol,
-  CContainer,
-  CFormInput,
-  CInputGroup,
-  CInputGroupText,
-  CRow,
-} from '@coreui/react'
-import CIcon from '@coreui/icons-react'
-import { cilMagnifyingGlass } from '@coreui/icons'
 
 const Page404 = () => {
   return (
@@ -19,18 +9,11 @@ const Page404 = () => {
           <CCol md={6}>
             <div className="clearfix">
               <h1 className="float-start display-3 me-4">404</h1>
-              <h4 className="pt-3">Oops! You{"'"}re lost.</h4>
+              <h4 className="pt-3">Opa! essa página não existe :(</h4>
               <p className="text-body-secondary float-start">
-                The page you are looking for was not found.
+                A página que você inseriu não existe.
               </p>
             </div>
-            <CInputGroup className="input-prepend">
-              <CInputGroupText>
-                <CIcon icon={cilMagnifyingGlass} />
-              </CInputGroupText>
-              <CFormInput type="text" placeholder="What are you looking for?" />
-              <CButton color="info">Search</CButton>
-            </CInputGroup>
           </CCol>
         </CRow>
       </CContainer>
