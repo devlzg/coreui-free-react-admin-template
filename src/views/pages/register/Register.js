199046435c23317cb37b36509cb7382d6278ca74
Luiz Santos <lzg.lisboasantos5569@gmail.com>
2025-03-19 15:52:28 -0300

    atualizando página de cadastro

--- src/views/pages/register/Register.js
+++ src/views/pages/register/Register.js
@@ -1,4 +1,4 @@
-import { cilLockLocked, cilUser } from '@coreui/icons'
+import { cilLockLocked, cilScreenSmartphone, cilUser } from '@coreui/icons'
 import CIcon from '@coreui/icons-react'
 import {
   CButton,
@@ -6,6 +6,10 @@ import {
   CCardBody,
   CCol,
   CContainer,
+  CDropdown,
+  CDropdownItem,
+  CDropdownMenu,
+  CDropdownToggle,
   CForm,
   CFormInput,
   CInputGroup,
@@ -22,23 +26,53 @@ const formatCPF = (value) => {
     .replace(/(\d{3})(\d{1,2})$/, '$1-$2') // Coloca um traço antes dos últimos 2 dígitos
 }
 
+const formatNumTel = (value) => {
+  return value
+    .replace(/\D/g, '') // Remove tudo que não for dígito
+    .replace(/^(\d{2})(\d)/, '($1) $2') // Coloca parênteses em volta dos dois primeiros dígitos (DDD)
+    .replace(/(\d{5})(\d)/, '$1-$2') // Coloca um hífen depois do quinto dígito
+}
+
 const removeMask = (value) => {
   return value.replace(/\D/g, '') // Remove todos os caracteres não numéricos
 }
 
 const Register = () => {
   const [cpf, setCpf] = useState('')
+  const [nome, setNome] = useState('')
+  const [userRole, setUserRole] = useState('')
+  const [userNumTel, setUserNumTel] = useState('')
+  const [senha, setSenha] = useState('')
+  const [email, setEmail] = useState('')
 
-  const handleChange = (e) => {
+  const handleChangeCpf = (e) => {
     const rawValue = e.target.value
     setCpf(formatCPF(rawValue)) // Atualiza o estado com a máscara aplicada
+  }
+
+  const handleChangeTel = (e) => {
+    const rawValue = e.target.value
+    setUserNumTel(formatNumTel(rawValue)) // Atualiza o estado com a máscara aplicada
   }
 
   const handleSubmit = (e) => {
     e.preventDefault()
     const cpfSemMascara = removeMask(cpf)
-    console.log('CPF enviado:', cpfSemMascara) // Enviar este valor para o backend
+    const numTelSemMascara = removeMask(userNumTel)
+    const dadosUsuario = {
+      cpf: cpfSemMascara,
+      nome: nome,
+      telefone: numTelSemMascara,
+      email: email,
+      senha: senha,
+      cargo: userRole,
+    }
+    console.log(dadosUsuario)
   }
+  const handleDropdown = (role) => {
+    setUserRole(role)
+  }
+
   return (
     <div className="bg-body-tertiary min-vh-100 d-flex flex-row align-items-center">
       <CContainer>
@@ -57,17 +91,47 @@ const Register = () => {
                       type="text"
                       maxLength="14"
                       value={cpf}
-                      onChange={handleChange}
-                      placeholder="Digite seu CPF"
+                      onChange={handleChangeCpf}
+                      placeholder="CPF"
                       name="registerCpfForm"
+                      required
+                    />
+                  </CInputGroup>
+                  <CInputGroup className="mb-3">
+                    <CInputGroupText>
+                      <CIcon icon={cilUser} />
+                    </CInputGroupText>
+                    <CFormInput
+                      type="text"
+                      placeholder="Nome"
+                      name="nome"
+                      required
+                      value={nome}
+                      onChange={(e) => setNome(e.target.value)}
+                    />
+                  </CInputGroup>
+                  <CInputGroup className="mb-3">
+                    <CInputGroupText>
+                      <CIcon icon={cilScreenSmartphone} />
+                    </CInputGroupText>
+                    <CFormInput
+                      type="text"
+                      placeholder="Telefone"
+                      name="telefone"
+                      maxLength="15"
+                      onChange={handleChangeTel}
+                      value={userNumTel}
                       required
                     />
                   </CInputGroup>
                   <CInputGroup className="mb-3">
                     <CInputGroupText>@</CInputGroupText>
                     <CFormInput
+                      type="email"
                       placeholder="Email"
                       autoComplete="email"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
                       name="registerEmailForm"
                       required
                     />
@@ -79,6 +143,8 @@ const Register = () => {
                     <CFormInput
                       type="password"
                       placeholder="Senha"
+                      value={senha}
+                      onChange={(e) => setSenha(e.target.value)}
                       name="registerSenhaform"
                       required
                     />
@@ -94,8 +160,30 @@ const Register = () => {
                       required
                     />
                   </CInputGroup>
+                  <CDropdown>
+                    {userRole != '' ? (
+                      <CDropdownToggle color="primary">{userRole}</CDropdownToggle>
+                    ) : (
+                      <CDropdownToggle color="primary">Função</CDropdownToggle>
+                    )}
+                    <CDropdownMenu>
+                      <CDropdownItem onClick={() => handleDropdown('Usuário')}>
+                        Usuário
+                      </CDropdownItem>
+                      <CDropdownItem onClick={() => handleDropdown('Administrador')}>
+                        Administrador
+                      </CDropdownItem>
+                      <CDropdownItem onClick={() => handleDropdown('Super Usuário')}>
+                        Super Usuário
+                      </CDropdownItem>
+                    </CDropdownMenu>
+                  </CDropdown>
                   <div className="d-grid">
-                    <CButton color="success" type="submit" style={{ color: 'white' }}>
+                    <CButton
+                      color="success"
+                      type="submit"
+                      style={{ color: 'white', marginTop: '12px' }}
+                    >
                       Criar conta
                     </CButton>
                   </div>
