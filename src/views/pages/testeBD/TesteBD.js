03588f8efabcacca64e6bb4ddd90a8df68c21b70
Luiz Santos <lzg.lisboasantos5569@gmail.com>
2025-03-20 16:36:39 -0300

    testando banco de dados

---
+++ src/views/pages/testeBD/TesteBD.js
@@ -0,0 +1,34 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+
+function TesteBD() {
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    // Fetch users from the backend
+    axios
+      .get('http://localhost:5000/api/usuarios')
+      .then((response) => {
+        setUsers(response.data.users)
+      })
+      .catch((error) => {
+        console.error('Error fetching users:', error)
+      })
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  return (
+    <div>
+      <h1>Usuarios</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.nome} - {user.email}
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default TesteBD
